ce49502452f3478f7d57402478002a77c6255410
Mike M <mikal815@users.noreply.github.com>
2019-02-10 21:28:51 -0500

    Add user schema

--- server/server.js
+++ server/server.js
@@ -9,9 +9,24 @@ require('dotenv').config();
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE);
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+// Models
+
+const { User } = require('./models/user');
+
+//=================================
+//             USERS 
+//=================================
+
+app.post('/api/users/register',(req,res)=>{
+    
+    res.status(200);
+})
+
+
 
 const port = process.env.PORT || 3002;
 
