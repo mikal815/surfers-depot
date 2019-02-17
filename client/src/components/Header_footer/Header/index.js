c1d605ed171d30945135eb64430e7bccb52073b5
Mike M <mikal815@users.noreply.github.com>
2019-02-17 17:43:58 -0500

    Begin routing

---
+++ client/src/components/Header_footer/Header/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="bck_b_light">
+                <div className="container">
+                    <div className="left">
+                        <div className="logo">
+                            SURFER'S DEPOT
+                        </div>
+
+                    </div>
+                    <div className="right">
+                        <div className="top">
+                            LINKS
+                        </div>
+                        <div className="bottom">
+                            LINKS
+                        </div>
+
+                    </div>
+
+                </div>
+
+                
+            </header>
+        );
+    }
+}
+
+export default Header;
