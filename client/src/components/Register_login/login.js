5ebca7744ab131fbcab865ea5159dac81bacbd4c
Mike M <mikal815@users.noreply.github.com>
2019-02-22 00:11:16 -0500

    Finish logging in user through form

--- client/src/components/Register_login/login.js
+++ client/src/components/Register_login/login.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import FormField from '../utils/Form/formfield';
 import { update, generateData, isFormValid } from '../utils/Form/formActions';
+import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { loginUser } from '../../actions/user_actions';
 
 class Login extends Component {
 
@@ -60,7 +62,16 @@ class Login extends Component {
         let formIsValid = isFormValid(this.state.formdata,'login')
 
         if(formIsValid){
-            console.log(dataToSubmit)
+            this.props.dispatch(loginUser(dataToSubmit)).then(response =>{
+                if(response.payload.loginSuccess){
+                    console.log(response.payload);
+                    this.props.history.push('/user/dashboard')
+                }else{
+                    this.setState({
+                        formError: true
+                    })
+                }
+            });
         } else {
             this.setState({
                 formError: true
@@ -101,4 +112,4 @@ class Login extends Component {
     }
 }
 
-export default connect()(Login);
+export default connect()(withRouter(Login));
