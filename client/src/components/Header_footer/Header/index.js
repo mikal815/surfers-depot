85a9fb5404b91eaeb0feb50d71d29a17baff1851
Mike M <mikal815@users.noreply.github.com>
2019-02-24 18:28:34 -0500

    Set state for header links & Send data to UserLayout

--- client/src/components/Header_footer/Header/index.js
+++ client/src/components/Header_footer/Header/index.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+
+    state = {
+        page: [
+            {
+                name:'Home',
+                linkTo: '/',
+                public: true
+            },
+            {
+                name:'Surfboards',
+                linkTo: '/shop',
+                public: true
+            }
+        ],
+        user: [
+            {
+                name:'My Cart',
+                linkTo: '/user/cart',
+                public: false
+            },
+            {
+                name:'My Account',
+                linkTo: '/user/dashboard',
+                public: false
+            },
+            {
+                name:'Log in',
+                linkTo: '/register_login',
+                public: true
+            },
+            {
+                name:'Log out',
+                linkTo: '/user/logout',
+                public: false
+            }
+        ]
+    }
+
     render() {
         return (
             <header className="bck_b_light">
