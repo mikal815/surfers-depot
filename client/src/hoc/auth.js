68a7ae4932b1af2d82472e8dcf1263dac3ac3c58
Mike M <mikal815@users.noreply.github.com>
2019-02-24 15:03:48 -0500

    Begin unauthorized route prevention/map state to props

---
+++ client/src/hoc/auth.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { auth } from '../actions/user_actions';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
+export default function(ComposedClass,reload,adminRoute = null){
+    class AuthenticationCheck extends Component {
+
+        state = {
+            loading: true
+        }
+
+        render() {
+            if(this.state.loading){
+                return (
+                    <div className="main_loader">
+                        <CircularProgress style={{color:'#2196F3'}} thickness={7}/>
+                    </div>
+                )
+            }
+            return (
+                <ComposedClass {...this.props} user={this.props.user}/>
+            );
+        }
+    }
+    
+    function mapStateToProps(state){
+        return {
+            user: state.user
+        }
+    }
+
+    return connect(mapStateToProps)(AuthenticationCheck)
+}
+
