2f14d97242c374d93e486506b372990f6c7e90c3
Mike M <mikal815@users.noreply.github.com>
2019-03-01 23:02:43 -0500

    Setup products actions and reducer

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import HomeSlider from './home_slider';
 import HomePromotion from './home_promotion';
 
+import { connect } from 'react-redux';
+import { getProductsBySell, getProductsByArrival } from '../../actions/products_actions';
+
 class Home extends Component {
+
+    componentDidMount(){
+        this.props.dispatch()
+    }
+
     render() {
         return (
             <div>
@@ -13,4 +21,10 @@ class Home extends Component {
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        products: state.products
+    }
+}
+
+export default connect(mapStateToProps)(Home);
