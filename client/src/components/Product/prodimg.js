51661f51defa3be97ae2d28d3341cafa8f0cd39a
Mike M <mikal815@users.noreply.github.com>
2019-03-13 00:46:13 -0400

    Display all product images under seleted image

--- client/src/components/Product/prodimg.js
+++ client/src/components/Product/prodimg.js
@@ -21,6 +21,23 @@ class ProdImg extends Component {
         }
     }
 
+    handleLightBox = () => {
+
+    }
+
+    showThumbs = () => (
+        this.state.lightboxImages.map((item,i)=>(
+            i > 0 ?
+                <div
+                    key={i}
+                    onClick={()=> this.handleLightBox(i)}
+                    className="thumb"
+                    style={{background:`url(${item}) no-repeat`}}
+                ></div>
+            :null
+        ))
+    )
+
     renderCardImage = (images) => {
         if(images.length > 0){
             return images[0].url
@@ -36,10 +53,14 @@ class ProdImg extends Component {
                 <div className="main_pic">
                     <div
                         style={{background:`url(${this.renderCardImage(detail.images)}) no-repeat`}}
+                        onClick={()=> this.handleLightBox(0)}
                     >
 
                     </div>
 
+                </div>
+                <div className="main_thumbs">
+                    {this.showThumbs(detail)}
                 </div>
             </div>
         );
