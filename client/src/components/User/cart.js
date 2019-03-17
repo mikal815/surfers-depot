2efad0aacf1fcafd216eb6697721abc09d849124
Mike M <mikal815@users.noreply.github.com>
2019-03-17 18:52:49 -0400

    Calculate and display total price of combined items in cart

--- client/src/components/User/cart.js
+++ client/src/components/User/cart.js
@@ -31,11 +31,26 @@ class UserCart extends Component {
 
                 this.props.dispatch(getCartItems(cartItems,user.userData.cart))
                 .then(()=>{
-
+                    if(this.props.user.cartDetail.length > 0){
+                        this.calculateTotal(this.props.user.cartDetail);
+                    }
                 })
 
             }
         }
+    }
+
+    calculateTotal = (cartDetail) => {
+        let total = 0;
+
+        cartDetail.forEach(item=>{
+            total += parseInt(item.price, 10) * item.quantity
+        });
+
+        this.setState({
+            total,
+            showTotal: true
+        });
     }
 
     removeFromCart = () => {
@@ -53,6 +68,16 @@ class UserCart extends Component {
                             type="cart"
                             removeItem={(id)=> this.removeFromCart(id)}
                         />
+                        {this.state.showTotal ?
+                            <div>
+                                <div className="user_cart_sum">
+                                    <div>
+                                        Total amount: $ {this.state.total}
+                                    </div>
+                                </div>
+                            </div>
+                    
+                        :null}
                     </div>
                 </div>
             </UserLayout>
