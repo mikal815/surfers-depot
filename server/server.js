f62cfc2989af55d3477138a4d4c9100c063782d9
Mike M <mikal815@users.noreply.github.com>
2019-03-30 18:03:01 -0400

    Config Heroku build variables

--- server/server.js
+++ server/server.js
@@ -9,11 +9,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -353,6 +355,14 @@ app.get('/api/users/removeFromCart',auth,(req,res)=>{
     }
     )
 })
+
+// Default
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 3002;
 
