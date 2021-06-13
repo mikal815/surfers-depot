02849da320dcabeed17bc496e28188a3211af562
Mike M <mikal815@users.noreply.github.com>
2021-06-13 18:59:33 -0400

    Revert

--- server/server.js
+++ server/server.js
@@ -369,15 +369,6 @@ app.get('/api/users/removeFromCart', auth, (req, res) => {
 // };
 // app.use(cors(corsOptions));
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = process.env.MONGODB_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
-
 
 
 
